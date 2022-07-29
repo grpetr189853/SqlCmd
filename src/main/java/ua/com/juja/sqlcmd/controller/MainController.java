eec2d6a988870dba8c2485953ac5b57936fd636e
grpetr189853 <grpetr189853@gmail.com>
2022-07-29 20:42:35 +0300

    SqlCmd check input parameters length is done

--- src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
+++ src/main/java/ua/com/juja/sqlcmd/controller/MainController.java
@@ -23,23 +23,30 @@ public class MainController {
         view.write("Привет юзер!");
         view.write("Введи пожалуйста имя базы данных, пользователя и пароль в формате: database|userName|password");
         while(true) {
-            String string = view.read();
-            String[] data = string.split("\\|");
-            String databaseName = data[0];
-            String userName = data[1];
-            String password = data[2];
             try {
+                String string = view.read();
+                String[] data = string.split("\\|");
+                if (data.length != 3){
+                    throw new IllegalAccessException("Неверное количество параметров разделенных знаком '|' ожидается 3, но есть : " + data.length);
+                }
+                String databaseName = data[0];
+                String userName = data[1];
+                String password = data[2];
                 manager.connect(databaseName, userName, password);
                 break;
             } catch (Exception e) {
-                String message = e.getMessage();
-                if(message != null){
-                    message += " " + e.getCause().getMessage();
-                }
-                view.write("Неудача! по причине: " + message);
-                view.write("Повторите попытку!");
+                printError(e);
             }
         }
         view.write("Подключились");
+    }
+
+    private void printError(Exception e) {
+        String message = e.getMessage();
+        if(e.getCause() != null){
+            message += " " + e.getCause().getMessage();
+        }
+        view.write("Неудача! по причине: " + message);
+        view.write("Повторите попытку!");
     }
 }
